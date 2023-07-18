f46ab6fe1939470ddb20089a42692fafe3e27a00
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2023-07-18 09:14:34 -0400

    update

--- api/routes/princilias.js
+++ api/routes/princilias.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { updateOne } = require("../models/princilia");
 const router = express.Router();
 const Princilia = require("../models/princilia");
 
 router.get("/", (req, res, next) => {
   res.json({
     Message: "Princilias - GET",
+    _id: Princilia,
   });
+  newPrincilia
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Princilia Saved",
+        book: {
+          title: result.title,
+          author: result.author,
+          id: result._id,
+          metadata: {
+            method: req.method,
+            host: req.hostname,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -50,8 +77,32 @@ router.get("/:princiliaId", (req, res, next) => {
     Message: "Princilias - GET",
     id: princiliaId,
   });
+  newPrincilia
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Princilia Saved",
+        book: {
+          title: result.title,
+          author: result.author,
+          id: result._id,
+          metadata: {
+            method: req.method,
+            host: req.hostname,
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
-
 router.patch("/:princiliaId", (req, res, next) => {
   const princiliaId = req.params.princiliaId;
 
@@ -67,6 +118,35 @@ router.delete("/:princiliaId", (req, res, next) => {
     Message: "Princilias - DELETE",
     id: princiliaId,
   });
+  Princilia.updateOne(
+    {
+      _id: princiliaId,
+    },
+    {
+      $set: updatePrincilia,
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        message: "Update Princilia",
+        Princilia: {
+          title: result.title,
+          author: result.author,
+          id: result._id,
+        },
+        metadata: {
+          host: req.hostname,
+          method: req.method,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
 module.exports = router;
